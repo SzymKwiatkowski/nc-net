0fcd673d9bac2936bb4f40dead5d5d185ae3c075
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2024-04-21 12:40:39 +0200

    feat: exporting model along with model assesment (#14)

--- src/export.py
+++ src/export.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import onnx
 
 from models.model import ControllerModel
-from utils.model_utils import export_onnx
+from utils.model_utils import export_onnx, measure_latency, get_size
 
 
 def export_model(args):
@@ -12,15 +12,19 @@ def export_model(args):
     torch_model = ControllerModel.load_from_checkpoint(model_path)
     onnx_export_name = args.export_name
     extraction_points = args.extraction_points_count
-    input_size = (extraction_points+1) * 8
-    # dummy_input = torch.randn(1, input_size)
-    #
-    # torch.onnx.export(torch_model, dummy_input, onnx_export_name, input_names=['input'],
-    #                   output_names=['output'], export_params=True)
+    input_size = args.input_size
+    assessment = args.assessment
+    if input_size == 0 or input_size is None:
+        input_size = (extraction_points+1) * 7
+
     export_onnx(torch_model, input_size=input_size, onnx_model_name=onnx_export_name)
 
     onnx_model = onnx.load(onnx_export_name)
     onnx.checker.check_model(onnx_model)
+
+    if assessment:
+        print("Model size: ", get_size(onnx_export_name))
+        print("Average infer time: ", measure_latency(torch_model, input_size))
 
 
 if __name__ == '__main__':
@@ -33,5 +37,10 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--data-path', action='store', default='../data/sample_data.csv')
     parser.add_argument('-ep', '--extraction-points-count', action='store', default=20,
                         type=int, help='Specified count of points from trajectory to be used')
+    parser.add_argument('-ip', '--input-size', action='store', type=int,
+                        help='Specified input size if passed to script automatically overrides' +
+                             ' extraction points (whole pose is taken into account)')
+    parser.add_argument('-a', '--assessment', action='store', type=bool, default=False,
+                        help='Specifies whether to assess model performance')
     parsed_args = parser.parse_args()
     export_model(parsed_args)
